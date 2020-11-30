e4bd30fb2e46c6bf09fe4d494b7eaa0de18403d9
Mukesh smk <38074577+mukeshsmk@users.noreply.github.com>
2020-11-30 18:24:52 +0530

    Add files via upload

---
+++ dynamic-table.ts
@@ -0,0 +1,520 @@
+healthTable = {
+    'header': [
+      {
+        'primaryKey': 'targetname',
+        'displayName': 'Company',
+        'outputType': 'text',
+        style: { width: '12%' },
+      },
+      {
+        'primaryKey': '101',
+        'displayName': 'COB',
+        'outputType': 'progress-bar',
+        style: { width: '30%' },
+      },
+      {
+        'primaryKey': '102',
+        'displayName': 'System Difference',
+        'outputType': 'text',
+        style: { width: '15%' },
+      },
+      {
+        'primaryKey': '103',
+        'displayName': 'Error',
+        'outputType': 'text',
+        style: { width: '10%' },
+      },
+    ],
+    'tableData': [
+      [
+        { 'key': 'targetname', 'value': 'NL0010001', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 95,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 0,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010002', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 45,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 0,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '2', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010003', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 30,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 0,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010004', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 95,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 0,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010005', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 10,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '4', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010006', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 80,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 0,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 0,
+              'unit': '%',
+            }
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '2', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010007', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '3', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010008', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL0010009', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '2', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL00100010', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL00100011', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '1', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL00100012', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '4', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL00100013', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '2', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'N', 'unit': 'percentage' },
+      ],
+      [
+        { 'key': 'targetname', 'value': 'NL00100014', 'unit': 'string' },
+        {
+          'key': '101', 'value': [
+            {
+              'stage': 'A',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'S',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'R',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'D',
+              'value': 100,
+              'unit': '%',
+            },
+            {
+              'stage': 'O',
+              'value': 100,
+              'unit': '%',
+            },
+          ],
+          'unit': 'percentage'
+        },
+        { 'key': '102', 'value': '3', 'unit': 'percentage' },
+        { 'key': '103', 'value': 'Y', 'unit': 'percentage' },
+      ],
+    ],
+  };
